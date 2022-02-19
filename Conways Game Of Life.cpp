f1f6f193efbb4048b4501ac1a3c74417d7179ed2
babaid <de.babai2000@gmail.com>
2022-02-19 23:07:12 +0100

    Projektdateien hinzufügen.

---
+++ Conways Game Of Life.cpp
@@ -0,0 +1,12 @@
+﻿// Conways Game Of Life.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Conways Game Of Life.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
